c23e549bc92c805ecd9bac2cfc73271ca1d27ce6
rohit93c <rohit93c@gmail.com>
2017-10-17 12:23:28 +0530

    added page load comment

--- App_Code/LearnEFModel.Designer.cs
+++ App_Code/LearnEFModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Rohit\source\repos\Demo\App_Code\LearnEFModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Rohit\source\repos\LearnEF\App_Code\LearnEFModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
